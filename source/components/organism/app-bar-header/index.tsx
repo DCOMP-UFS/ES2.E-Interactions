fd45b6ae2a880fbdb178f0dbfb51fb6cc1e2df00
murilomontino <murilomontinojr@hotmail.com>
2022-05-09 16:36:19 -0300

    [component] app bar

---
+++ source/components/organism/app-bar-header/index.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/prop-types */
+import { Appbar } from 'react-native-paper'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+import theme from '@/theme'
+import { AntDesign } from '@expo/vector-icons'
+
+import { useDrawer } from '@/context/drawer-context'
+
+type Props = {
+  navigation?: any
+  back?: any
+  options?: any
+  [key: string]: any
+}
+
+const CustomNavigationBar = ({ navigation, back, options }: Props) => {
+  /*   const [visible, setVisible] = useState(false)
+
+  const openMenu = () => setVisible(true)
+  const closeMenu = () => setVisible(false)
+ */
+  const { openDrawer } = useDrawer()
+
+  return (
+    <Appbar.Header
+      style={{
+        backgroundColor: theme.COLORS.TOPBAR,
+      }}
+    >
+      <Appbar.Action
+        icon={() => <AntDesign name="menuunfold" size={RFValue(10)} color="white" />}
+        onPress={openDrawer}
+      />
+      <Appbar.Content
+        title={options.tabBarLabel}
+        titleStyle={{
+          textTransform: 'lowercase',
+          fontVariant: ['small-caps'],
+          fontSize: RFValue(10),
+          fontWeight: 'bold',
+          textAlign: 'center',
+        }}
+      />
+      <Appbar.Action icon="menu" color="transparent" />
+    </Appbar.Header>
+  )
+}
+
+export default CustomNavigationBar
