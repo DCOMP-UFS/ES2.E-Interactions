32496e2939b055dd16e7bf99e4cc66cd94e3d29c
murilomontino <murilomontinojr@hotmail.com>
2022-05-09 19:29:05 -0300

    [component] card de seact

---
+++ source/components/molecule/selectable-option/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Pressable } from 'react-native'
+
+import { FontAwesome } from '@expo/vector-icons'
+
+import { Drug, CardPressable } from './styles'
+
+type Item = {
+  id: number
+  name: string
+}
+
+type Props = {
+  item: Item
+  onPress?: (item: Item) => void
+  defaultSelected?: boolean
+}
+
+const SelectableOption = ({ item, onPress, defaultSelected = false }: Props) => {
+  const [selected, setSelected] = useState(defaultSelected)
+
+  const handlePress = () => {
+    setSelected(!selected)
+    if (onPress) onPress(item)
+  }
+
+  return (
+    <Pressable style={CardPressable} onPress={handlePress}>
+      <Drug>{item.name}</Drug>
+      <FontAwesome name="check-circle" size={24} color={selected ? '#00a680' : '#ccc'} />
+    </Pressable>
+  )
+}
+
+export default SelectableOption
---
+++ source/components/molecule/selectable-option/styles.ts
@@ -0,0 +1,27 @@
+import { StyleProp, ViewStyle } from 'react-native'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+import styled from 'styled-components/native'
+
+export const Drug = styled.Text`
+  font-size: ${RFValue(12)}px;
+  font-weight: 800;
+  font-variant: small-caps;
+  text-transform: capitalize;
+  color: #000;
+`
+
+export const CardPressable = ({ pressed }) =>
+  ({
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: pressed ? '#dfdfdf' : '#fff',
+    padding: 16,
+    borderRadius: 4,
+    marginTop: 4,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ccc',
+    elevation: 5,
+  } as StyleProp<ViewStyle>)
