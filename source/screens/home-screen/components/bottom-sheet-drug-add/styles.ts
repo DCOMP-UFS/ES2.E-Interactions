8a4bc00db5821933a241a52da09a14fd36ba06fd
murilomontino <murilomontinojr@hotmail.com>
2022-05-09 16:37:59 -0300

    [component] bottom sheet drug add

---
+++ source/screens/home-screen/components/bottom-sheet-drug-add/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const PosicionarBotaoVolta = styled.TouchableOpacity`
+  align-items: left;
+  width: 32px;
+  flex-direction: row;
+`
+
+export const TituloUm = styled.Text`
+  padding-left: 16px;
+  font-size: 28px;
+  font-weight: bold;
+`
