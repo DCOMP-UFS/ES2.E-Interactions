626b9b625c1ce15624d04f5fdd228dd368b79609
murilomontino <murilomontinojr@hotmail.com>
2022-05-11 14:06:59 -0300

    [refactor] correções de desempenho e melhorias

--- source/components/organism/input-search-dropdown/index.tsx
+++ source/components/organism/input-search-dropdown/index.tsx
@@ -1,31 +1,35 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react'
-import { Button, FlatList, View } from 'react-native'
+import React, { useMemo, useState } from 'react'
+import { Button, FlatList, View, ActivityIndicator } from 'react-native'
 import { Searchbar } from 'react-native-paper'
+
+import { drugs } from '@/data/drugs.json'
 
 import SelectableOption from '@/components/molecule/selectable-option'
 
-import { drugs } from './drugs.json'
 import { Container, ContainerButtons } from './styles'
 
+drugs.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
+
+const SUM_MIN_ITEMS = 5
+
 const InputSearchDropdown = () => {
-  const [data, setData] = useState(
-    drugs.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
-  )
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState(drugs)
+  const [itemsPerPage, setItemsPerPage] = useState(10)
   const [searchQuery, setSearchQuery] = useState('')
 
   const [selectedItem, setSelectedItem] = useState([])
 
   const onChangeSearch = (query: string) => setSearchQuery(query)
 
-  useEffect(() => {
-    setData(() => {
-      const searchLowerCase = searchQuery.toLowerCase()
-      const newDataAll = drugs.filter((item) => item.name.toLowerCase().includes(searchLowerCase))
-      const filtered = newDataAll.filter((item) => !selectedItem.includes(item))
-      return filtered
-    })
-  }, [searchQuery])
+  const sliceData = useMemo(() => {
+    if (searchQuery.trim().length === 0) {
+      return data.slice(0, itemsPerPage)
+    }
+
+    return data.filter((item) => item.name.includes(searchQuery)).slice(0, itemsPerPage)
+  }, [data, searchQuery, itemsPerPage])
 
   const onChangeSelection = (item) => {
     setSelectedItem((state) => [...state, item])
@@ -38,6 +42,22 @@ const InputSearchDropdown = () => {
     setSelectedItem((state) => state.filter((i) => i.id !== item.id))
     setData((state) =>
       [...state, item].sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
+    )
+  }
+
+  const loadData = async () => {
+    setLoading(true)
+    await new Promise((resolve) => setTimeout(resolve, 200))
+    setItemsPerPage(itemsPerPage + SUM_MIN_ITEMS)
+    setLoading(false)
+  }
+
+  const renderFooter = () => {
+    if (!loading) return null
+    return (
+      <View>
+        <ActivityIndicator />
+      </View>
     )
   }
 
@@ -73,7 +93,10 @@ const InputSearchDropdown = () => {
             paddingBottom: 200,
           }}
           initialNumToRender={10}
-          data={data}
+          data={sliceData}
+          onEndReached={loadData}
+          ListFooterComponent={renderFooter}
+          onEndReachedThreshold={0.1}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => <SelectableOption item={item} onPress={onChangeSelection} />}
         />
